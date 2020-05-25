f686344c277382def25a22cc7fd3381b0c266269
shankho <sharanyo111@gmail.com>
2020-05-25 23:59:00 +0530

    hi

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+app.get('/article-one', function(req, res){
+	res.sendFile(path.join(__dirname,'ui','one.html'));
+});
+app.get('/article-two', function(req, res){
+	res.send("Try 2");
+});
+app.get('/article-three', function(req, res){
+	res.send("Try 3");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
